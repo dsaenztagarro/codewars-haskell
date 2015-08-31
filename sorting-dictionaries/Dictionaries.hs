41662b67bc43fb6f15d25c3405e5a1d67ead4c28
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-08-31 16:30:58 +0200

    New implementation "sorting dictionaries"

--- sorting-dictionaries/Dictionaries.hs
+++ sorting-dictionaries/Dictionaries.hs
@@ -1,17 +1,29 @@
 module Dictionaries where
 
 sortDict :: Ord v => [(k,v)] -> [(k,v)]
-sortDict dict = reverse $ qsort dict
+sortDict dict = qsort dict
 
-qsort :: Ord v => [(k,v)] -> [(k,v)]
-qsort [] = []
-qsort (x:xs) = qsort lesser ++ equal ++ qsort greater
+-- implementation 1
+
+qsort' :: Ord v => [(k,v)] -> [(k,v)]
+qsort' [] = []
+qsort' (x:xs) = qsort' lesser ++ equal ++ qsort' greater
   where
     (lesser,equal,greater) = part x xs ([],[x],[])
 
 part :: Ord v => (k,v) -> [(k,v)] -> ([(k,v)],[(k,v)],[(k,v)]) -> ([(k,v)],[(k,v)],[(k,v)])
 part _ [] (l,e,g) = (l,e,g)
 part p@(k,v) (x@(k',v'):xs) (l,e,g)
-    | v' > v     = part p xs (l,e,x:g)
-    | v' < v     = part p xs (x:l,e,g)
+    | v' > v     = part p xs (l ++ [x],e,g)
+    | v' < v     = part p xs (l,e,g ++ [x])
     | otherwise = part p xs (l,e ++ [x],g)
+
+-- implementation 2
+
+qsort :: Ord v => [(k,v)] -> [(k,v)]
+qsort [] = []
+qsort (x@(_,v):xs) = (qsort greater) ++ [x] ++ equals ++ (qsort lesser)
+  where
+    lesser = filter (\(_,v') -> v' < v) xs
+    greater = filter (\(_,v') -> v' > v) xs
+    equals = filter (\(_,v') -> v' == v) xs
--- sorting-dictionaries/Dictionaries/Test.hs
+++ sorting-dictionaries/Dictionaries/Test.hs
@@ -9,7 +9,10 @@ main = hspec $ do
       sortDict [(3,1),(2,2),(1,3)] `shouldBe` [(1,3),(2,2),(3,1)]
       sortDict [(1,2),(2,4),(3,6)] `shouldBe` [(3,6),(2,4),(1,2)]
       sortDict [(0,0),(0,6),(1,6)] `shouldBe` [(0,6),(1,6),(0,0)]
+      sortDict [(0,3),(1,3),(0,0)] `shouldBe` [(0,3),(1,3),(0,0)]
+      sortDict [(0,1),(1,1)] `shouldBe` [(0,1),(1,1)]
 
     it "should handle dictionaries with strings as keys" $ do
       sortDict [("a", 6), ("b", 2), ("c",4)] `shouldBe` [("a", 6), ("c", 4), ("b",2)]
+
 
