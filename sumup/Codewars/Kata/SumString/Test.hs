99c31d77578699924ec376c2c3b58fdf0b395746
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-09-04 00:53:38 +0200

    Added sum up the random string

---
+++ sumup/Codewars/Kata/SumString/Test.hs
@@ -0,0 +1,16 @@
+module Codewars.Kata.SumString.Test (main) where
+import Codewars.Kata.SumString (sumFromString)
+import Test.Hspec
+import Test.QuickCheck
+
+main = hspec $ describe "sumFromString" $ do
+  let testSum str n = it ("returns the correct value for " ++ show str) $ sumFromString str `shouldBe` n
+
+  testSum ""       0
+  testSum "In 2015, how expensive is an i386?" 2401
+  testSum "1+1=2"  4
+  testSum "E=mc^2" 2
+
+  it "returns the correct number on a string consisting only of numbers" $ property $ \xs ->
+    let s = sum . map abs $ xs
+    in sumFromString (unwords . map show $ xs) `shouldBe` s
