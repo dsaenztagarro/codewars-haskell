000b097214da7f75637dc9bb580648fd3bb7e411
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-09-04 02:34:02 +0200

    OMG the haskell way for sumup

--- sumup/Codewars/Kata/SumString.hs
+++ sumup/Codewars/Kata/SumString.hs
@@ -1,9 +1,6 @@
 module Codewars.Kata.SumString where
-
-import Text.Regex.Posix
+import Data.List.Split
+import Data.Char
 
 sumFromString :: String -> Integer
-sumFromString xs = sum numbers
-    where
-      matches = xs =~ "[0-9]+" :: [[String]]
-      numbers = map (\xs -> (read . head $ xs) :: Integer) matches
+sumFromString = sum . map read . wordsBy (not . isDigit)
